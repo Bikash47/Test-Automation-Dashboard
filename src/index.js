9a5c6c8f53d35055362cb6b53cb42821fc29b883
Bikash Kumar Sahu <bikashkumarsahu9@gmail.com>
2019-11-21 18:13:24 +0530

    Index changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './assets/style.css'
+import './assets/style.css';
 import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
 import Main from "./Main";
